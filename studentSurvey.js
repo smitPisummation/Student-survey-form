02dc84cd6f853c1c6edba156e087f4bbf2dbc543
Smit <smit.isummation@outlook.com>
2024-01-24 18:17:30 +0530

    Student survey form submited

---
+++ studentSurvey.js
@@ -0,0 +1,7 @@
+function funAreaFocus(className) {
+    document.getElementById(className).style.backgroundColor = 'rgb(250, 248, 217)'
+}
+
+function funAreaBlur(className) {
+    document.getElementById(className).style.backgroundColor = 'transparent'
+}
